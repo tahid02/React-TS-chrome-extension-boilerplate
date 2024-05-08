fded6a350002024698124de49a0b4a5c243896b3
tahid02 <tanzim1463@gmail.com>
2024-05-09 04:12:13 +0600

    static file copy by webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // this is actually a node.js file
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/test.tsx", // the base file ( where other files were imported)
@@ -12,6 +13,18 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    // it can handle anything that module cant do
+    new CopyPlugin({
+      // manifest.json is static here, it has no imported files, so we directly copy it in dist folder
+      patterns: [
+        {
+          from: path.resolve("src/manifest.json"), //
+          to: path.resolve("dist"),
+        },
+      ],
+    }),
+  ],
   resolve: {
     extensions: [".tsx", ".ts", ".js"], // work on imported files for these files
   },
